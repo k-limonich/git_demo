bbbeb55e5fcb00a79a6fae50e4c32481ecf59bf4
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-08-16 00:41:17 +0300

    Create Faculty class unit test

--- Java/Error&Exception/src/test/java/com/epam/training/client/FacultyTest.java
+++ Java/Error&Exception/src/test/java/com/epam/training/client/FacultyTest.java
@@ -20,23 +20,21 @@ public class FacultyTest extends AbstractBaseTest {
 	@Test
 	void testAddNewStudent() {
 		Faculty faculty = university.getFaculties()[COMPUTER_AIDED_DESIGN.ordinal()];
-		Student newStudent = new Student("Ilya Ivanov", 19, 2,
-				COMPUTER_AIDED_DESIGN, GroupNumber.FIRST,
-				Map.of(PHYSICS, 5, ENGLISH, 6));
+		Student newStudent = new Student("Ilya Ivanov", COMPUTER_AIDED_DESIGN,
+				GroupNumber.FIRST, Map.of(PHYSICS, 5, ENGLISH, 6));
 		List<Student> expectedStudents = new ArrayList<>(faculty.getStudents());
 		expectedStudents.add(newStudent);
-		university.addStudent(newStudent);
+		university.add(newStudent);
 		assertEquals(expectedStudents, faculty.getStudents());
 	}
 
 	@Test
 	void testAddExistingStudent() {
 		Faculty faculty = university.getFaculties()[COMPUTER_AIDED_DESIGN.ordinal()];
-		Student newStudent = new Student("Boris Borisov", 18, 1,
-				COMPUTER_AIDED_DESIGN, GroupNumber.SECOND,
-				Map.of(PROGRAMMING, 7, ENGINEERING_DRAWING, 5, ENGLISH, 8));
+		Student newStudent = new Student("Boris Borisov", COMPUTER_AIDED_DESIGN,
+				GroupNumber.SECOND, Map.of(PROGRAMMING, 7, ENGINEERING_DRAWING, 5));
 		List<Student> expectedStudents = new ArrayList<>(faculty.getStudents());
-		university.addStudent(newStudent);
+		university.add(newStudent);
 		assertEquals(expectedStudents, faculty.getStudents());
 	}
 }
