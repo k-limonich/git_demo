84a84037ea0c5c07e734266b67c520e32dad5b3b
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-09-06 00:01:01 +0300

    Tune project

---
+++ Java/Threads/OptionalTask/src/test/java/com/epam/training/RunnerTest.java
@@ -0,0 +1,13 @@
+package com.epam.training;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RunnerTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
