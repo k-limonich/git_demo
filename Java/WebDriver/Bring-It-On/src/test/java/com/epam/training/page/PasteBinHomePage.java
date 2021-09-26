e1c262973fdad7cdc655ae17336701821de5e5da
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-09-26 22:07:31 +0300

    Create pastebin homepage class according to PageObject model

---
+++ Java/WebDriver/Bring-It-On/src/test/java/com/epam/training/page/PasteBinHomePage.java
@@ -0,0 +1,92 @@
+package com.epam.training.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import com.epam.training.waits.CustomConditions;
+
+public class PasteBinHomePage extends AbstractPage {
+
+	private static String HOMEPAGE_URL = "https://pastebin.com";
+
+	@FindBy(tagName = "textarea")
+	private WebElement textInput;
+
+	@FindBy(xpath = "//div[contains(@class, 'expiration')]/descendant::span[@role='presentation']")
+	private WebElement expirationTimeDropDownList;
+
+	@FindBy(xpath = "//div[contains(@class, 'format')]/descendant::span[@role='presentation']")
+	private WebElement syntaxHighlightingDropDownList;
+
+	@FindBy(xpath = "//div[@class='toggle__control']")
+	private WebElement syntaxHighlightingToggle;
+
+	@FindBy(xpath = "//input[@type='text' and contains(@id, 'name')]")
+	private WebElement pasteNameInput;
+
+	@FindBy(xpath = "//button[@type='submit']")
+	private WebElement createPasteButton;
+
+	private final String dropDownListOptionDefaultLocator = "//*[@role='option' and text()='%s']";
+
+	public PasteBinHomePage(WebDriver driver) {
+		super(driver);
+		driver.manage().window().maximize();
+	}
+
+	@Override
+	public PasteBinHomePage openPage() {
+		driver.get(HOMEPAGE_URL);
+		new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+				.until(CustomConditions.jQueryAJAXsCompleted());
+		return this;
+	}
+
+	public PasteBinHomePage enterPaste(String paste) {
+		textInput.sendKeys(paste);
+		return this;
+	}
+
+	public PasteBinHomePage selectSyntaxHighlighting(String highlightingLanguage) {
+		scrollPage(200);
+		new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+				.until(ExpectedConditions.elementToBeClickable(syntaxHighlightingDropDownList))
+				.click();
+		new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+				.until(ExpectedConditions.presenceOfElementLocated(
+						By.xpath(buildDropDownListOptionLocator(highlightingLanguage))))
+				.click();
+		return this;
+	}
+
+	public PasteBinHomePage enableSyntaxHighlighting() {
+		scrollPage(150);
+		syntaxHighlightingToggle.click();
+		return this;
+	}
+
+	public PasteBinHomePage selectExpirationTime(String expirationTime) {
+		scrollPage(300);
+		new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+				.until(ExpectedConditions.elementToBeClickable(expirationTimeDropDownList))
+				.click();
+		new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+				.until(ExpectedConditions.presenceOfElementLocated(
+						By.xpath(buildDropDownListOptionLocator(expirationTime))))
+				.click();
+		return this;
+	}
+
+	private String buildDropDownListOptionLocator(String optionName) {
+		return String.format(dropDownListOptionDefaultLocator, optionName);
+	}
+
+	public SuccessfulPasteCreationPage createNewPaste(String pasteName) {
+		pasteNameInput.sendKeys(pasteName);
+		createPasteButton.click();
+		return new SuccessfulPasteCreationPage(driver, pasteName);
+	}
+}
---
+++ Java/WebDriver/I-Can-Win/src/test/java/com/epam/training/page/PasteBinHomePage.java
@@ -0,0 +1,78 @@
+package com.epam.training.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import com.epam.training.waits.CustomConditions;
+
+import java.util.List;
+
+public class PasteBinHomePage {
+
+	private static int WAIT_TIMEOUT_SECONDS = 10;
+	private static String HOMEPAGE_URL = "https://pastebin.com";
+
+	private WebDriver driver;
+
+	@FindBy(tagName = "textarea")
+	private WebElement textInput;
+
+	@FindBy(xpath = "//div[contains(@class, 'expiration')]/descendant::span[@role='presentation']")
+	private WebElement expirationTimeDropDownList;
+
+	@FindBy(xpath = "//input[@type='text' and contains(@id, 'name')]")
+	private WebElement pasteNameInput;
+
+	@FindBy(xpath = "//button[@type='submit']")
+	private WebElement createPasteButton;
+
+	private final String expirationTimeDefaultLocator = "//*[@role='listbox']/*[contains(., '%s')]";
+	private final String successNoticeLocator = "//div[contains(@class, 'success')]";
+
+	public PasteBinHomePage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	public PasteBinHomePage openPage() {
+		driver.get(HOMEPAGE_URL);
+		new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+				.until(CustomConditions.jQueryAJAXsCompleted());
+		return this;
+	}
+
+	public PasteBinHomePage enterPaste(String paste) {
+		textInput.sendKeys(paste);
+		return this;
+	}
+
+	public PasteBinHomePage selectExpirationTime(String expirationTimeOption) {
+		expirationTimeDropDownList.click();
+		new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+				.until(ExpectedConditions.elementToBeClickable(
+						By.xpath(buildExpirationTimeLocator(expirationTimeOption))))
+				.click();
+		return this;
+	}
+
+	private String buildExpirationTimeLocator(String expirationTimeOption) {
+		return String.format(expirationTimeDefaultLocator, expirationTimeOption);
+	}
+
+	public PasteBinHomePage enterPasteName(String pasteName) {
+		pasteNameInput.sendKeys(pasteName);
+		return this;
+	}
+
+	public boolean createNewPaste() {
+		createPasteButton.click();
+		new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath(successNoticeLocator)));
+		List<WebElement> successNotice = driver.findElements(By.xpath(successNoticeLocator));
+		return !successNotice.isEmpty();
+	}
+}
