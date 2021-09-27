caa6ca1467b7687e223c1b330c3e58a79284692a
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-09-27 18:49:30 +0300

    Remove unused imports

--- Java/WebDriver/Bring-It-On/src/test/java/com/epam/training/waits/CustomConditions.java
+++ Java/WebDriver/Bring-It-On/src/test/java/com/epam/training/waits/CustomConditions.java
@@ -1,9 +1,7 @@
 package com.epam.training.waits;
 
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.FluentWait;
 
 public class CustomConditions {
 
