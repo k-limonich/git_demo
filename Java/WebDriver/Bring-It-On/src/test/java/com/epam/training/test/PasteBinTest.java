b8861ea10f6f22ace0c3c95164ed80be10153b39
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-09-27 18:51:21 +0300

    Rename field, add methods and assertions description

--- Java/WebDriver/Bring-It-On/src/test/java/com/epam/training/test/PasteBinTest.java
+++ Java/WebDriver/Bring-It-On/src/test/java/com/epam/training/test/PasteBinTest.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 public class PasteBinTest {
 
 	private WebDriver driver;
-	private SuccessfulPasteCreationPage page;
+	private SuccessfulPasteCreationPage createdPastePage;
 
 	private static String expectedRawPasteData = "git config --global user.name  \"New Sheriff in Town\"\n" +
 			"git reset $(git commit-tree HEAD^{tree} -m \"Legacy code\")\n" +
@@ -21,15 +21,15 @@ public class PasteBinTest {
 	private static String expectedExpirationTime = "10 Minutes";
 	private static String pasteTitle = "how to gain dominance among developers";
 
-	@BeforeClass(alwaysRun = true)
+	@BeforeClass(description = "Chrome browser setup", alwaysRun = true)
 	public void browserSetUp() {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	}
 
-	@BeforeClass(alwaysRun = true)
+	@BeforeClass(description = "Transition to page after paste creation", alwaysRun = true)
 	public void pageSetUp() {
-		page = new PasteBinHomePage(driver)
+		createdPastePage = new PasteBinHomePage(driver)
 				.openPage()
 				.enterPaste(expectedRawPasteData)
 				.enableSyntaxHighlighting()
@@ -39,26 +39,29 @@ public class PasteBinTest {
 				.waitUntilLoaded();
 	}
 
-	@Test(description = "Page title test")
+	@Test(description = "Page title is correct test")
 	public void testNewPasteTitleIsCorrect() {
-		String expectedPageTitle = pasteTitle + " - Pastebin.com";
-		String actualPageTitle = driver.getTitle();
-		Assert.assertEquals(actualPageTitle, expectedPageTitle);
+		Assert.assertEquals(driver.getTitle(),
+						pasteTitle + " - Pastebin.com",
+						"Title of page with created paste doesn't match paste title");
 	}
 
-	@Test(description = "Bash syntax highlighting test")
-	public void testBashSyntaxHighlightingIsOn() {
-		String actualSyntaxHighlightingLanguage = page.determineSyntaxHighlightingLanguage();
-		Assert.assertEquals(actualSyntaxHighlightingLanguage, expectedSyntaxHighlightingLanguage);
+	@Test(description = "Bash syntax highlighting is enabled test")
+	public void testSyntaxHighlightingIsBash() {
+		Assert.assertEquals(createdPastePage.determineSyntaxHighlightingLanguage(),
+							expectedSyntaxHighlightingLanguage,
+						"Paste syntax highlighting isn't Bash");
 	}
 
-	@Test(description = "Raw paste data test")
+	@Test(description = "Raw paste data is correct test")
 	public void testRawPasteDataIsCorrect() {
-		String actualRawPasteData = page.getRawPasteData();
-		Assert.assertEquals(actualRawPasteData, expectedRawPasteData);
+		Assert.assertEquals(createdPastePage.getRawPasteData(),
+							expectedRawPasteData,
+						"Raw paste data doesn't match the data, which was entered" +
+								" while creating the paste");
 	}
 
-	@AfterClass(alwaysRun = true)
+	@AfterClass(description = "Chrome browser teardown", alwaysRun = true)
 	public void browserTearDown() {
 		driver.quit();
 		driver = null;
