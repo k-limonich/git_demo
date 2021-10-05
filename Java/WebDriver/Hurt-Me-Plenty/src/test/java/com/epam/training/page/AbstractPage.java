f4d08aaf5ff05cb1389066e01ebb3a34a64e870b
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-10-05 22:33:50 +0300

    Create base abstract page

---
+++ Java/WebDriver/Hurt-Me-Plenty/src/test/java/com/epam/training/page/AbstractPage.java
@@ -0,0 +1,22 @@
+package com.epam.training.page;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class AbstractPage {
+
+	protected static int WAIT_TIMEOUT_SECONDS = 10;
+
+	protected WebDriver driver;
+	protected WebDriverWait wait;
+
+	protected AbstractPage(WebDriver driver) {
+		this.driver = driver;
+		wait = new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS);
+		PageFactory.initElements(driver, this);
+	}
+
+	protected abstract AbstractPage openPage();
+}
