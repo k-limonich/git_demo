31c5b3abf7a5ad01384a44dd4688c2dbcab4e360
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-10-15 19:51:51 +0300

    Create google search results page

---
+++ Java/WebDriver/Hardcore/src/test/java/com/epam/training/page/google/SearchResultsPage.java
@@ -0,0 +1,29 @@
+package com.epam.training.page.google;
+
+import com.epam.training.page.AbstractPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class SearchResultsPage extends AbstractPage {
+
+	private final String searchTerm;
+
+	private final static String DEFAULT_SEARCH_RESULT_LOCATOR = "//a[@class='gs-title' and contains(., '%s')]";
+
+	public SearchResultsPage(WebDriver driver, String searchTerm) {
+		super(driver);
+		this.searchTerm = searchTerm;
+	}
+
+	public PriceCalculatorPage goToPricingCalculatorPage() {
+		wait.until(ExpectedConditions.presenceOfElementLocated(
+						By.xpath(buildSearchResultLocator())))
+				.click();
+		return new PriceCalculatorPage(driver);
+	}
+
+	private String buildSearchResultLocator() {
+		return String.format(DEFAULT_SEARCH_RESULT_LOCATOR, searchTerm);
+	}
+}
