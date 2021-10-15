3b603d27ff059cb85b0b5f3250183f11dfdf89de
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-10-15 19:55:50 +0300

    Create YOPmail email generator page

---
+++ Java/WebDriver/Hardcore/src/test/java/com/epam/training/page/yopmail/EmailGeneratorPage.java
@@ -0,0 +1,31 @@
+package com.epam.training.page.yopmail;
+
+import com.epam.training.page.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class EmailGeneratorPage extends AbstractPage {
+
+	@FindBy(id = "egen")
+	private WebElement generatedEmail;
+
+	@FindBy(xpath = "//span[text()='Check Inbox']")
+	private WebElement btnCheckInbox;
+
+	protected EmailGeneratorPage(WebDriver driver) {
+		super(driver);
+	}
+
+	public String copyEmailAddress() {
+		return generatedEmail.getText();
+	}
+
+	public InboxPage checkInbox() {
+		wait.until(ExpectedConditions.elementToBeClickable(btnCheckInbox))
+				.click();
+		return new InboxPage(driver);
+	}
+
+}
