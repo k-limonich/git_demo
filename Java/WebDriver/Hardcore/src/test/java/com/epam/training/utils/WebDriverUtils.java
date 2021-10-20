ffc6f199c7c6480974e6d127a11063b106ab3676
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-10-20 18:02:10 +0300

    Remove iframe related methods

--- Java/WebDriver/Hardcore/src/test/java/com/epam/training/utils/WebDriverUtils.java
+++ Java/WebDriver/Hardcore/src/test/java/com/epam/training/utils/WebDriverUtils.java
@@ -2,30 +2,11 @@ package com.epam.training.utils;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebDriverUtils {
 
 	private static final int WAIT_TIMEOUT_SECONDS = 10;
-
-	/**
-	 * Switches to a single or nested iframe element
-	 *
-	 * @param driver WebDriver object
-	 * @param frames iframe elements array, driver will switch to the last element in the array
-	 */
-	public static void switchToIFrame(WebDriver driver, WebElement ...frames) {
-		for (WebElement frame : frames) {
-			new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-					.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(frame));
-		}
-	}
-
-	public static void switchToMainFrame(WebDriver driver) {
-		driver.switchTo().defaultContent();
-	}
 
 	public static void openBlankTab(WebDriver driver) {
 		((JavascriptExecutor) driver).executeScript("window.open('about:blank','_blank');");
@@ -38,7 +19,7 @@ public class WebDriverUtils {
 		for (String windowHandle : driver.getWindowHandles()) {
 			if (!originalHandle.equals(windowHandle)) {
 				driver.switchTo().window(windowHandle);
-				return;
+				break;
 			}
 		}
 	}
