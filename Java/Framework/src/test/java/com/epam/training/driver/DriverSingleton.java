05d65f158345819b116f0be374c5986e48e44894
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-10-24 13:18:53 +0300

    Add webdrivermanager

---
+++ Java/Framework/src/test/java/com/epam/training/driver/DriverSingleton.java
@@ -0,0 +1,32 @@
+package com.epam.training.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverSingleton {
+
+	private static WebDriver driver;
+
+	private DriverSingleton() {}
+
+	public static WebDriver getDriver() {
+		if (driver == null) {
+			if (System.getProperty("browser").equals("firefox")) {
+				WebDriverManager.firefoxdriver().setup();
+				driver = new FirefoxDriver();
+			} else {
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+			}
+			driver.manage().window().maximize();
+		}
+		return driver;
+	}
+
+	public static void closeDriver() {
+		driver.quit();
+		driver = null;
+	}
+}
