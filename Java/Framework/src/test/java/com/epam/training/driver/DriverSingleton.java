62c447af47c98530f1792be584236a0005f1d073
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-10-24 17:24:02 +0300

    Add edge browser property parameter

--- Java/Framework/src/test/java/com/epam/training/driver/DriverSingleton.java
+++ Java/Framework/src/test/java/com/epam/training/driver/DriverSingleton.java
@@ -3,6 +3,7 @@ package com.epam.training.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverSingleton {
@@ -16,6 +17,9 @@ public class DriverSingleton {
 			if (System.getProperty("browser").equals("firefox")) {
 				WebDriverManager.firefoxdriver().setup();
 				driver = new FirefoxDriver();
+			} else if (System.getProperty("browser").equals("edge")) {
+				WebDriverManager.edgedriver().setup();
+				driver = new EdgeDriver();
 			} else {
 				WebDriverManager.chromedriver().setup();
 				driver = new ChromeDriver();
