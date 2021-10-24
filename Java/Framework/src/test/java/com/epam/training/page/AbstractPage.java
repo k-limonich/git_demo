cec97a25deddad379facbe43561229399175679b
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-10-24 13:14:23 +0300

    Create base abstract class for page objects

---
+++ Java/Framework/src/test/java/com/epam/training/page/AbstractPage.java
@@ -0,0 +1,35 @@
+package com.epam.training.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class AbstractPage {
+
+	protected static final int WAIT_TIMEOUT_SECONDS = 10;
+
+	protected WebDriver driver;
+
+	protected AbstractPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	/**
+	 * Switches to a single or nested iframe element
+	 *
+	 * @param frames iframe elements array, driver will switch to the last element in the array
+	 */
+	protected void switchToIFrame(WebElement...frames) {
+		for (WebElement frame : frames) {
+			new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+					.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(frame));
+		}
+	}
+
+	protected void switchToMainFrame() {
+		driver.switchTo().defaultContent();
+	}
+}
