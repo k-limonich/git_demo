8e01b18995f7de9ae532570dbad0fc653d861c2e
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-10-24 13:17:56 +0300

    Create class with field names for PriceCalculatorPage

---
+++ Java/Framework/src/test/java/com/epam/training/constants/Fields.java
@@ -0,0 +1,16 @@
+package com.epam.training.constants;
+
+public class Fields {
+
+	public static final String OPERATING_SYSTEM = "Operating System";
+	public static final String MACHINE_CLASS = "Machine Class";
+	public static final String SERIES = "Series";
+	public static final String MACHINE_TYPE = "Machine type";
+	public static final String NUMBER_OF_GPUS = "Number of GPUs";
+	public static final String GPU_TYPE = "GPU type";
+	public static final String LOCAL_SSD = "Local SSD";
+	public static final String DATACENTER_LOCATION = "Datacenter location";
+	public static final String COMMITTED_USAGE = "Committed usage";
+
+	private Fields() {}
+}
