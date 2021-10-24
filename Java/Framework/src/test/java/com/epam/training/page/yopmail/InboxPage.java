612ae365e08c02414dccf3d5740fb385d0d96267
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-10-24 16:32:56 +0300

    Change getTotalCost method return type to primitive double, change selectTotalCostEmail method

--- Java/Framework/src/test/java/com/epam/training/page/yopmail/InboxPage.java
+++ Java/Framework/src/test/java/com/epam/training/page/yopmail/InboxPage.java
@@ -2,18 +2,17 @@ package com.epam.training.page.yopmail;
 
 import com.epam.training.page.AbstractPage;
 import com.epam.training.util.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class InboxPage extends AbstractPage {
 
-	private final Logger logger = LogManager.getRootLogger();
 	private final static String TOTAL_COST_REGEX = "(?<=USD\\s)[0-9.]+";
 
 	@FindBy(id = "ifinbox")
@@ -22,45 +21,43 @@ public class InboxPage extends AbstractPage {
 	@FindBy(id = "ifmail")
 	private WebElement iframeEmailContent;
 
-	@FindBy(xpath = "//div[text()='Google Cloud Platform Price Estimate']/parent::button")
-	private WebElement priceEstimateEmail;
-
 	@FindBy(id = "refresh")
 	private WebElement buttonRefresh;
 
 	@FindBy(xpath = "//table/tbody/tr/td/h3[contains(., 'USD')]")
 	private WebElement headerTotalCost;
 
+	private static final By BY_TOTAL_COST_EMAIL_LOCATOR = By
+			.xpath("//div[text()='Google Cloud Platform Price Estimate']/parent::button");
+
 	protected InboxPage(WebDriver driver) {
 		super(driver);
 	}
 
-	public InboxPage refreshInbox() {
-		new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-				.until(ExpectedConditions.elementToBeClickable(buttonRefresh))
-				.click();
-		return this;
-	}
-
-	public InboxPage selectPriceEstimateEmail() {
-		switchToIFrame(iframeInbox);
-		new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-				.until(ExpectedConditions.elementToBeClickable(priceEstimateEmail))
-				.click();
+	public InboxPage selectTotalCostEmail() {
+		for (int refreshCounter = 0; refreshCounter < 5; refreshCounter++) {
+			switchToIFrame(iframeInbox);
+			List<WebElement> totalCostEmail = driver.findElements(BY_TOTAL_COST_EMAIL_LOCATOR);
+			if (totalCostEmail.isEmpty()) {
+				switchToMainFrame();
+				refreshInbox();
+			} else break;
+		}
 		switchToMainFrame();
 		return this;
 	}
 
-	public Double getTotalCost() {
-		try {
-			switchToIFrame(iframeEmailContent);
-			String totalCostString = StringUtils
-					.extractSubstring(headerTotalCost.getText(), TOTAL_COST_REGEX);
-			switchToMainFrame();
-			return Double.parseDouble(totalCostString);
-		} catch (NoSuchElementException e) {
-			logger.error("Failed to find header containing total cost on email inbox page");
-		}
-		return null;
+	private void refreshInbox() {
+		new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+				.until(ExpectedConditions.elementToBeClickable(buttonRefresh))
+				.click();
+	}
+
+	public double getTotalCost() {
+		switchToIFrame(iframeEmailContent);
+		String totalCostString = StringUtils
+				.extractSubstring(headerTotalCost.getText(), TOTAL_COST_REGEX);
+		switchToMainFrame();
+		return Double.parseDouble(totalCostString);
 	}
 }
