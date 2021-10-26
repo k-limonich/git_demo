fdb8a03489f0b0861c520010469fcf82f084fd8b
Aleksei Klimovich <klimovich_alexsei@mail.ru>
2021-10-26 22:11:57 +0300

    Make static fields final

--- Java/WebDriver/Bring-It-On/src/test/java/com/epam/training/test/PasteBinTest.java
+++ Java/WebDriver/Bring-It-On/src/test/java/com/epam/training/test/PasteBinTest.java
@@ -14,12 +14,12 @@ public class PasteBinTest {
 	private WebDriver driver;
 	private SuccessfulPasteCreationPage createdPastePage;
 
-	private static String expectedRawPasteData = "git config --global user.name  \"New Sheriff in Town\"\n" +
+	private static final String EXPECTED_RAW_PASTE_DATA = "git config --global user.name  \"New Sheriff in Town\"\n" +
 			"git reset $(git commit-tree HEAD^{tree} -m \"Legacy code\")\n" +
 			"git push origin master --force";
-	private static String expectedSyntaxHighlightingLanguage = "Bash";
-	private static String expectedExpirationTime = "10 Minutes";
-	private static String pasteTitle = "how to gain dominance among developers";
+	private static final String EXPECTED_SYNTAX_HIGHLIGHTING_LANGUAGE = "Bash";
+	private static final String EXPECTED_EXPIRATION_TIME = "10 Minutes";
+	private static final String PASTE_TITLE = "how to gain dominance among developers";
 
 	@BeforeClass(description = "Chrome browser setup", alwaysRun = true)
 	public void browserSetUp() {
@@ -31,32 +31,32 @@ public class PasteBinTest {
 	public void pageSetUp() {
 		createdPastePage = new PasteBinHomePage(driver)
 				.openPage()
-				.enterPaste(expectedRawPasteData)
+				.enterPaste(EXPECTED_RAW_PASTE_DATA)
 				.enableSyntaxHighlighting()
-				.selectSyntaxHighlighting(expectedSyntaxHighlightingLanguage)
-				.selectExpirationTime(expectedExpirationTime)
-				.createNewPaste(pasteTitle)
+				.selectSyntaxHighlighting(EXPECTED_SYNTAX_HIGHLIGHTING_LANGUAGE)
+				.selectExpirationTime(EXPECTED_EXPIRATION_TIME)
+				.createNewPaste(PASTE_TITLE)
 				.waitUntilLoaded();
 	}
 
 	@Test(description = "Page title is correct test")
 	public void testNewPasteTitleIsCorrect() {
 		Assert.assertEquals(driver.getTitle(),
-						pasteTitle + " - Pastebin.com",
+						PASTE_TITLE + " - Pastebin.com",
 						"Title of page with created paste doesn't match paste title");
 	}
 
 	@Test(description = "Bash syntax highlighting is enabled test")
 	public void testSyntaxHighlightingIsBash() {
 		Assert.assertEquals(createdPastePage.determineSyntaxHighlightingLanguage(),
-							expectedSyntaxHighlightingLanguage,
+				EXPECTED_SYNTAX_HIGHLIGHTING_LANGUAGE,
 						"Paste syntax highlighting isn't Bash");
 	}
 
 	@Test(description = "Raw paste data is correct test")
 	public void testRawPasteDataIsCorrect() {
 		Assert.assertEquals(createdPastePage.getRawPasteData(),
-							expectedRawPasteData,
+				EXPECTED_RAW_PASTE_DATA,
 						"Raw paste data doesn't match the data, which was entered" +
 								" while creating the paste");
 	}
